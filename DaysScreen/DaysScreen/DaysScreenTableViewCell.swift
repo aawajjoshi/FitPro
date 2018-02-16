ee40b098267fb631747e9ee925f605b5377904d2
Sirisha Vinukonda <31740152+S530491@users.noreply.github.com>
2018-02-15 20:53:33 -0600

    Add files via upload
    Days Screen Created

---
+++ DaysScreen/DaysScreen/DaysScreenTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DaysScreenTableViewCell.swift
+//  DaysScreen
+//
+//  Created by Vinukonda,Sirisha on 2/15/18.
+//  Copyright © 2018 Vinukonda,Sirisha. All rights reserved.
+//
+
+import UIKit
+
+class DaysScreenTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var DayLabel: UILabel!
+    @IBOutlet weak var CellView: UIView!
+    @IBOutlet weak var DayImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
