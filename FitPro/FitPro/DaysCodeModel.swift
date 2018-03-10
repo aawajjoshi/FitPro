99a53a53500b04a2a15e9f1828116107c470e26d
Joshi,Aawaj R <s521315@ch140004m.nwmissouri.edu>
2018-03-09 18:12:12 -0600

    Made the second view controller called BodyPartViewController. Also wrote the necessary codes to display the list of body parts in the view.

--- FitPro/FitPro/DaysCodeModel.swift
+++ FitPro/FitPro/DaysCodeModel.swift
@@ -13,13 +13,19 @@ class  DaysModel{
 
     var dayImage:[UIImage]
     
+    var body:[String]
+    var bodyImage:[UIImage]
+    
     init(){
       days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
     dayImage=[#imageLiteral(resourceName: "Sunday"),#imageLiteral(resourceName: "Monday"),#imageLiteral(resourceName: "Tuesday"),#imageLiteral(resourceName: "Wednesday"),#imageLiteral(resourceName: "Thursday"),#imageLiteral(resourceName: "Friday"),#imageLiteral(resourceName: "Saturday")]
+        body = ["Legs", "Arms", "Chest", "Back", "Cardio", "Yoga"]
+        bodyImage = [#imageLiteral(resourceName: "Legs"), #imageLiteral(resourceName: "Arms"), #imageLiteral(resourceName: "Chest"), #imageLiteral(resourceName: "Back"), #imageLiteral(resourceName: "Cardio"), #imageLiteral(resourceName: "Yoga")]
         
     }
     class func DaysFunc()->DaysModel{
         return DaysModel()
     }
     
+   
 }
